cc45678d0a618cde69ac452a17a8a90b1134b5ff
Kyle Bulloch & brian kropff <student@epicodus.com>
2015-03-23 09:48:19 -0700

    Add CategoryTest and first test

---
+++ tests/TaskTest.php
@@ -0,0 +1,36 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Task.php";
+    require_once "src/Category.php";
+
+    $DB = new PDO('pgsql:host=localhost;dbname=to_do_test');
+
+    class CategoryTest extends PHPUnit_Framework_TestCase
+    {
+        // protected function tearDown()
+        // {
+        //     Category::deleteAll();
+        //     Task::deleteAll();
+        // }
+
+        function testGetName()
+        {
+            //Arrange
+            $name = "Dungeons and Dragons";
+            $test_category = new Category($name);
+
+            //Act
+            $result = $test_category->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+
+    }
+
+?>
